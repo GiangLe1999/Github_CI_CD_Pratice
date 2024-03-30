8cc1fda600d97ab1349eba76190ac90dabd92bfd
Lê Thanh Giang <legiangbmt09@gmail.com>
2024-03-30 11:01:31 +0700

    Add new endpoint

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -1,11 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/checkstatus', (req, res, next) => {
-    res.status(200).json({
-        status: 'success',
-        message: 'api ok'
-    })
-})
+router.get("/checkstatus", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "api ok",
+  });
+});
+
+router.get("/api/users", (req, res, next) => {
+  const data = [
+    { name: "Giang", age: 25 },
+    { name: "Huy", age: 26 },
+  ];
+
+  res.status(200).json({
+    data,
+    status: "success",
+    message: "api ok",
+  });
+});
 
 module.exports = router;
